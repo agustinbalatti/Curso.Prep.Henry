a802b4b29e06fa2baea04f90af408bb7c3be6aae
agustinbalatti <agustinbalatti@gmail.com>
2021-07-07 18:33:21 -0300

    corrigiendo y practicando commitear

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -121,7 +121,7 @@ function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (num % 2 != 0) {
+  if (num % 2 !== 0) {
     return true;
   }
   return false;
