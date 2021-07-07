08dc85d18d85cf32c74d704f58cf7a7284fd38c5
agustinbalatti <agustinbalatti@gmail.com>
2021-07-07 19:10:28 -0300

    nueva correccion, y ultima

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -182,8 +182,7 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
-  var strExcl = str + '!';
-  return strExcl;
+  return str + '!';
 }
 
 function combinarNombres(nombre, apellido) {
